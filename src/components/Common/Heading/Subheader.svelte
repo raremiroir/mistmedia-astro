42d4f227e4d5acf06251e042c5a7e56f9dba372c
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 19:06:50 +0100

    Add subtitle/subheader

---
+++ src/components/Common/Heading/Subheader.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+   import type { TitleSizes } from "./heading";
+   export let size:TitleSizes = "md" // xxs, xs, sm, md, lg, xl, xxl
+   export let color:string = ""
+   export let weight:string = ""
+   export let fontfam:string = ""
+   export let leading = ""
+   export let shadow = ""
+   
+   export let klass:string = ""
+
+   $: sizeClass = size === 'xxs' ? 'text-title-4xs'
+                : size === 'xs' ? 'text-title-3xs'
+                : size === 'sm' ? 'text-title-2xs'
+                : size === 'lg' ? 'text-title-sm'
+                : size === 'xl' ? 'text-title-md'
+                : size === 'xxl' ? 'text-title-lg'
+                : 'text-title-xs';
+   $: colorClass = color ? color : "text-black dark:text-white";
+   $: weightClass = weight ? weight : "font-bold";
+   $: fontfamClass = fontfam ? fontfam : "font-body";
+   $: leadingClass = leading ? leading : "";
+   $: shadowClass = shadow ? shadow : "drop-shadow-none";
+
+   export let transition = `transition-all duration-200 ease-out`;
+
+   $: titleClass = 
+         `unstyled ${sizeClass} ${colorClass} ${shadowClass} 
+         ${fontfamClass} ${leadingClass} ${weightClass} 
+         ${transition} ${klass}`
+</script>
+
+
+
+<span class="{titleClass}">
+   <slot/>
+</span>
---
+++ src/components/Common/Heading/Subtitle.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+   import type { TitleSizes } from "./heading";
+   export let size:TitleSizes = "md" // xs, sm, md, lg, xl, xxl
+   export let color:string = ""
+   export let weight:string = ""
+   export let fontfam:string = ""
+   export let leading = ""
+   export let shadow = ""
+   
+   export let klass:string = ""
+
+   $: sizeClass = size === 'xs' ? 'text-title-4xs'
+                : size === 'sm' ? 'text-title-3xs'
+                : size === 'lg' ? 'text-title-xs'
+                : size === 'xl' ? 'text-title-sm'
+                : size === 'xxl' ? 'text-title-md'
+                : 'text-title-2xs';
+   $: colorClass = color ? color : "text-gray-800 dark:text-gray-200";
+   $: weightClass = weight ? weight : "font-medium";
+   $: fontfamClass = fontfam ? fontfam : "font-body";
+   $: leadingClass = leading ? leading : "leading-snug";
+   $: shadowClass = shadow ? shadow : "drop-shadow-none";
+
+   export let transition = `transition-all duration-200 ease-out`;
+
+   $: titleClass = 
+         `unstyled ${sizeClass} ${colorClass} ${shadowClass} 
+         ${fontfamClass} ${leadingClass} ${weightClass} 
+         ${transition} ${klass}`
+</script>
+
+
+
+<span class="{titleClass}">
+   <slot/>
+</span>
