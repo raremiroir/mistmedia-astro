e5eba1de9973ed96e23af7568b81bea376c9c2f6
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 22:19:12 +0100

    Export components

--- src/components/svelte.ts
+++ src/components/svelte.ts
@@ -3,3 +3,8 @@
 // Common
 export { default as Heading } from './Common/Heading/Heading.svelte'
 export { default as Button } from './Common/Button/Button.svelte'
+// Main
+export { default as Hero } from './Main/Hero/Hero.svelte'
+
+// Media
+export { default as Svg } from './Media/Svg/Svg.svelte'
