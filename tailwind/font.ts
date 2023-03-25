480d1a4e34248ae0b72382445dfe64a987908f73
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 01:50:45 +0100

    Add tailwind theme config

---
+++ tailwind/font.ts
@@ -0,0 +1,12 @@
+const font = {
+   family: {
+      'body': ['quasimoda', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      'title': ['-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+      'titlemono': ['attribute-mono', 'monospace'],
+   },
+   size: {
+      '2xs': ['8px', '10px'],
+      '10xl': ['72px', '76px'],
+      '11xl': ['80px', '84px'],
+   }
+}
