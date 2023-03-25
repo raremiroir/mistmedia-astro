94e465eb4825b5b0c5139b9dfbc2eed281dabacf
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 16:03:41 +0100

    Add custom svg assets

---
+++ src/components/Media/Svg/svg-assets/MistMediaLogo.svelte
@@ -0,0 +1,43 @@
+<script>
+	export let klass = '';
+	export let color = 'fill-secondary-d3';
+	export let overlayColor = 'fill-secondary-d2';
+</script>
+
+<svg width="184" height="184" viewBox="0 0 184 184" fill="none" xmlns="http://www.w3.org/2000/svg" class={klass}>
+	<g id="MistMedia-Logo-Raw">
+		<g id="first-object">
+			<g id="first-object_2">
+				<path
+					d="M67 47.5C67 58.2696 58.2696 67 47.5 67C36.7304 67 28 58.2696 28 47.5C28 36.7304 36.7304 28 47.5 28C58.2696 28 67 36.7304 67 47.5Z"
+					class="{color}"
+				/>
+				<path d="M37.5 47H67V168H47L37.5 47Z" class="{color}" />
+				<path d="M28 28H47V168H28V28Z" class="{color}" />
+			</g>
+			<path id="bottom-shade" d="M67 146.5V168H28L67 146.5Z" class="{overlayColor}" />
+		</g>
+		<g id="second-object">
+			<g id="object">
+				<path
+					d="M112 72C112 83.0457 103.046 92 92 92C80.9543 92 72 83.0457 72 72C72 60.9543 80.9543 52 92 52C103.046 52 112 60.9543 112 72Z"
+					class="{color}"
+				/>
+				<path d="M82 71H112V156H92L82 71Z" class="{color}" />
+				<path d="M72 52H92V104V156H72V52Z" class="{color}" />
+			</g>
+			<path id="bottom-shade_2" d="M112 122.5V156H72V144L112 122.5Z" class="{overlayColor}" />
+		</g>
+		<g id="third-object">
+			<g id="object_2">
+				<path
+					d="M156 96.5C156 107.27 147.27 116 136.5 116C125.73 116 117 107.27 117 96.5C117 85.7304 125.73 77 136.5 77C147.27 77 156 85.7304 156 96.5Z"
+					class="{color}"
+				/>
+				<path d="M124 96H156V144H131L124 96Z" class="{color}" />
+				<path d="M117 77H136L131 144H117V77Z" class="{color}" />
+			</g>
+			<path id="bottom-shade_3" d="M117 119.5V144H156V98.5L117 119.5Z" class="{overlayColor}" />
+		</g>
+	</g>
+</svg>
--- src/components/Media/Svg/svg-assets/index.ts
+++ src/components/Media/Svg/svg-assets/index.ts
@@ -1,1 +1,8 @@
-export { default as MistLogo } from './MistLogo.svelte'
+export { default as AnimationDesignCube } from './AnimationDesignCube.svelte';
+export { default as DesignBranding } from './DesignBranding.svelte';
+// export { default as DevicesShowcase } from './DevicesShowcase.svelte';
+export { default as MistLogo } from './MistMediaLogo.svelte';
+export { default as MistMediaLogo } from './MistMediaLogo.svelte';
+export { default as WebDesignFullStack } from './WebDesignFullStack.svelte';
+export { default as WebDesignFullStackColor } from './WebDesignFullStackColor.svelte';
+export { default as WebDevCodeStack } from './WebDevCodeStack.svelte';
