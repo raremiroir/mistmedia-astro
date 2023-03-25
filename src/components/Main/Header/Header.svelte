a2fc79db8c1315fae3656171244f76b2879bcef0
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 02:57:47 +0100

    Sveltify header

---
+++ src/components/Main/Header/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+import HeaderLink from './HeaderLink.svelte';
+import { SITE_TITLE } from '../../../consts';
+import { AppBar } from '@skeletonlabs/skeleton';
+
+export let currentPath = '';
+$: currentPath;
+</script>
+
+<header class="
+         flex flex-row w-full 
+         justify-between items-center
+         mb-8">
+	<h2 class="my-2">
+		{SITE_TITLE}
+	</h2>
+	<AppBar gridColumns="grid-cols-3" slotDefault="place-self-center" slotTrail="place-content-end">
+		<svelte:fragment slot="lead">(icon)</svelte:fragment>
+		(title)
+		<svelte:fragment slot="trail">(actions)</svelte:fragment>
+	</AppBar>
+	<nav>
+		<HeaderLink {currentPath} href="/">Home</HeaderLink>
+		<HeaderLink {currentPath} href="/about">Over Ons</HeaderLink>
+		<HeaderLink {currentPath} href="/diensten">Onze Diensten</HeaderLink>
+		<HeaderLink {currentPath} href="/blog">Blog</HeaderLink>
+		<HeaderLink {currentPath} href="/contat">Contact</HeaderLink>
+	</nav>
+</header>
---
+++ src/components/Main/Header/HeaderLink.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+import type { HTMLAttributes } from 'astro/types';
+
+export let href:string = '';
+export let currentPath = '';
+
+let klass:string = '';
+export { klass as class};
+
+$: active = href === currentPath || href === currentPath.replace(/\/$/, '');
+</script>
+
+<a href={href} class="{klass}" class:active>
+	<span>
+		<slot />
+	</span>
+</a>
+
+<style>
+	a {
+		display: inline-block;
+		text-decoration: none;
+	}
+	a.active {
+		font-weight: bolder;
+		text-decoration: underline;
+	}
+</style>
