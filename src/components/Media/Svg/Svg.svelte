9eb8e3285d10de2e63b8d78f76a7a6d5408de7b4
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 16:04:05 +0100

    Integrate custom svg's

--- src/components/Media/Svg/Svg.svelte
+++ src/components/Media/Svg/Svg.svelte
@@ -1,18 +1,58 @@
 <script lang="ts">
-   import { MistLogo } from './svg-assets'
+   import MistLogo from './svg-assets/MistLogo.svelte';
+   import { 
+      AnimationDesignCube, DesignBranding, 
+      MistMediaLogo, 
+      WebDesignFullStack, WebDesignFullStackColor, WebDevCodeStack 
+   } from './svg-assets';
+   import {
+      Accessibility, ArrowsGrow, CaretDown, ChatBubble, CorporateBranding,
+      CubeIcon, DevicesResponsive, EnvelopeSend, FacebookIcon, GearsTurning, GithubIcon,
+      InstagramIcon, LangSwitchIcon, LightningBolt, LightWeight, LinkedInIcon, MistMediaIcon,
+      MoonIcon, PortfolioCards, PricingTag, QuestionMarks, Security, SunIcon, TrianglesIcon,
+      WebStack
+   } from './icon-animated'
 
    const svgAssets: {[key:string]: any} = {
-      MistLogo
+      AnimationDesignCube, DesignBranding, 
+      MistMediaLogo, WebDesignFullStack, WebDesignFullStackColor, 
+      WebDevCodeStack
+   }
+   const iconAnimationsAssets: {[key:string]: any} = {
+      Accessibility, ArrowsGrow, CaretDown, ChatBubble, CorporateBranding,
+      CubeIcon, DevicesResponsive, EnvelopeSend, FacebookIcon, GearsTurning, GithubIcon,
+      InstagramIcon, LangSwitchIcon, LightningBolt, LightWeight, LinkedInIcon, MistMediaIcon,
+      MoonIcon, PortfolioCards, PricingTag, QuestionMarks, Security, SunIcon, TrianglesIcon,
+      WebStack
    }
 
-   export let name: string = 'MistLogo'
-   export let width: number = 50;
+   export let size: number = 150;
 
+   export let logo = false;
+   export let svg = '';
+   export let animIcon = '';
+   
    let klass:string = '';
    export { klass as class }; 
 
-   $: svg = svgAssets[name]
+   $: svgComp = svg ? svgAssets[svg] : animIcon ? iconAnimationsAssets[animIcon] : logo ? MistLogo : MistLogo;
+
+   $: props = 
+         svg ? {} 
+       : animIcon ? {
+         size: size,
+         dark: 'fill-accent-d3 dark:fill-accent-l2',
+         norm: 'fill-accent-d1',
+         light: 'fill-accent',
+         strokeDark: 'stroke-accent-d3',
+         bgColor: 'primary',
+         darkBgColor: 'secondary-d1',
+         opacity: '70',
+       }
+       : logo ? {
+         width: size,
+       } : {}
 </script>
 
 
-<svelte:component this={svg} class={klass} {width}/>
+<svelte:component this={svgComp} class={klass} {...props}/>
