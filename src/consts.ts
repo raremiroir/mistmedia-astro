952fdabdfa04a0bef5c9116e1c40396055cddef8
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-25 22:16:30 +0100

    Add images

--- src/consts.ts
+++ src/consts.ts
@@ -3,3 +3,5 @@
 
 export const SITE_TITLE = 'My personal website.';
 export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const widths =  'mx-auto w-full sm:w-[95%] md:w-11/12 lg:w-5/6 xl:w-4/5 2xl:w-3/4';
