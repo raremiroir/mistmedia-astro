f6af6ac89eeb8939dbc3b5bf72f777bc66c4fc77
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-26 04:19:48 +0200

    Export card component

--- src/components/svelte.ts
+++ src/components/svelte.ts
@@ -3,6 +3,7 @@
 // Common
 export { default as Heading } from './Common/Heading/Heading.svelte'
 export { default as Button } from './Common/Button/Button.svelte'
+export { default as Card } from './Common/Card/Card.svelte'
 // Main
 export { default as Hero } from './Main/Hero/Hero.svelte'
 
