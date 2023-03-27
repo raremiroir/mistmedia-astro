64404bf44a99e5e20348b276d255d7038c803584
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-27 18:44:52 +0200

    Add type for exports in animProps

--- src/components/Media/Svg/Svg.svelte
+++ src/components/Media/Svg/Svg.svelte
@@ -28,11 +28,31 @@
       WebStack
    }
 
+   type AnimIconProps = {
+      dark?: string,
+      norm?: string,
+      light?: string,
+      strokeDark?: string,
+      bgColor?: string,
+      darkBgColor?: string,
+      opacity?: string,
+   }
+
    export let size: number = 150;
 
    export let logo = false;
    export let svg = '';
    export let animIcon = '';
+
+   export let animProps: AnimIconProps = {
+      dark: 'fill-primary-800 dark:fill-primary-400',
+      norm: 'fill-primary-600',
+      light: 'fill-primary',
+      strokeDark: 'stroke-primary-800',
+      bgColor: 'surface',
+      darkBgColor: 'surface-500',
+      opacity: '70',
+   }
    
    let klass:string = '';
    export { klass as class }; 
@@ -43,13 +63,13 @@
          svg ? {} 
        : animIcon ? {
          size: size,
-         dark: 'fill-primary-800 dark:fill-primary-400',
-         norm: 'fill-primary-600',
-         light: 'fill-primary',
-         strokeDark: 'stroke-primary-800',
-         bgColor: 'surface',
-         darkBgColor: 'surface-500',
-         opacity: '70',
+         dark: animProps.dark,
+         norm: animProps.norm,
+         light: animProps.light,
+         strokeDark: animProps.strokeDark,
+         bgColor: animProps.bgColor,
+         darkBgColor: animProps.darkBgColor,
+         opacity: animProps.opacity,
        }
        : logo ? {
          width: size,
