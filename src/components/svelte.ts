30010dd7f8347ec3f0c26e8f7b88c06448a7e001
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-27 12:30:32 +0200

    Export box component

--- src/components/svelte.ts
+++ src/components/svelte.ts
@@ -1,9 +1,12 @@
 // Base
+export { default as Box } from './Base/Box.svelte'
 
 // Common
 export { default as Heading } from './Common/Heading/Heading.svelte'
 export { default as Button } from './Common/Button/Button.svelte'
 export { default as Card } from './Common/Card/Card.svelte'
+
+
 // Main
 export { default as Hero } from './Main/Hero/Hero.svelte'
 
