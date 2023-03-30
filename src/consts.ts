8bf2546f319c637fc28dfb2990cdadd52de3f653
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-30 17:29:17 +0200

    Move consts to folder

--- src/consts.ts
+++
@@ -1,7 +0,0 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-
-export const widths =  'mx-auto w-full sm:w-[95%] md:w-11/12 lg:w-5/6 xl:w-4/5 2xl:w-3/4';
---
+++ src/consts/class.ts
@@ -0,0 +1,10 @@
+
+export const widths =  'mx-auto w-full sm:w-[95%] md:w-11/12 lg:w-5/6 xl:w-4/5 2xl:w-3/4';
+
+export const slider = `
+      w-full h-full max-h-120
+      object-cover relative 
+      cursor-grab active:cursor-grabbing 
+      py-2 rounded-2xl
+      bg-surface-400 overflow-hidden
+   `;
