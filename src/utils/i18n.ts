79f478f7042c2e84d99ab52970ee1b88e1f435ca
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-31 15:25:44 +0200

    Add consts for locale names and flags

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,7 +1,18 @@
 import type { Locales } from "@tstype/index";
+import { locale } from "@stores/index";
 
 // All locales
 export const locales: Locales[] = ['nl', 'en']
-export const localesAsString: string[] = ['nl', 'en']
 export const baseLocale: Locales = 'nl'
 
+export const localeFlags: {[key:string]: string} = {
+   nl: 'flag-for-netherlands',
+   en: 'flag-for-united-kingdom',
+   fr: 'flag-for-france',
+}
+
+export const localeNames: {[key:string]: string} = {
+   nl: 'Nederlands',
+   en: 'English',
+   fr: 'Français',
+}
