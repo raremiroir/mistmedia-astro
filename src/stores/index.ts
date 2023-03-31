99f81cdaac1ca537206ddeeb0e45eb5dc47b5651
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-31 15:30:40 +0200

    Add stores file

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,11 +1,4 @@
-import { atom } from 'nanostores'
-import { baseLocale } from '@utils/i18n'
-import type { Locales } from '@tstype/index';
-
 
 /* ~~-~~ ~-~~-~ ~~-~~ */
 /* ~~-~~ STORES ~~-~~ */
 /* ~~-~~ ~-~~-~ ~~-~~ */
-
-// i18n
-export let locale = atom<Locales>(baseLocale);
