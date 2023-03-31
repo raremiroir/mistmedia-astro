86f92fc0905aa7098e34b731cce2ca0331973f30
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-31 15:27:06 +0200

    Add store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,4 +1,11 @@
-import { writable } from "svelte/store";
-import type { Writable } from "svelte/store";
+import { atom } from 'nanostores'
+import { baseLocale } from '@utils/i18n'
+import type { Locales } from '@tstype/index';
 
+
+/* ~~-~~ ~-~~-~ ~~-~~ */
 /* ~~-~~ STORES ~~-~~ */
+/* ~~-~~ ~-~~-~ ~~-~~ */
+
+// i18n
+export let locale = atom<Locales>(baseLocale);
