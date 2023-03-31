1070203e0b8dc182947cbd4f6dac5cdccd80c630
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-03-31 12:10:09 +0200

    add scheme for blog posts

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,8 +1,11 @@
+// Import utilities from astro:content	
 import { defineCollection, z } from 'astro:content';
 
+// define schema for each collection
 const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
+		isDraft: z.boolean().optional(),
 		title: z.string(),
 		description: z.string(),
 		// Transform string to Date object
@@ -15,7 +18,19 @@ const blog = defineCollection({
 			.optional()
 			.transform((str) => (str ? new Date(str) : undefined)),
 		heroImage: z.string().optional(),
+		language: z.enum(['nl', 'en']).optional(),
+		tags: z.array(z.string()).optional(),
+		author: z.string().default(`${import.meta.env.AUTHOR_NAME}`),
+		authorImage: z.string().default('/public/' + `${import.meta.env.AUTHOR_IMAGE}`),
+		authorTwitter: z.string().default(`${import.meta.env.AUTHOR_TWITTER}`),
+		authorGithub: z.string().default(`${import.meta.env.AUTHOR_GITHUB}`),
+		authorLinkedIn: z.string().default(`${import.meta.env.AUTHOR_LINKEDIN}`),
+		authorWebsite: z.string().default(`${import.meta.env.AUTHOR_URL}`),
+		authorEmail: z.string().default(`${import.meta.env.AUTHOR_EMAIL}`),
 	}),
 });
 
-export const collections = { blog };
+// Export collections object to register in collection(s)
+export const collections = { 
+	blog
+};
