8c58114da8671a4d731c87e658d4c4f3724d495d
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-01 15:18:35 +0200

    Add props

--- src/components/Form/FormRow.svelte
+++ src/components/Form/FormRow.svelte
@@ -1,3 +1,11 @@
-<div class="w-full flex flex-row gap-2 justify-between items-center">
+<script>
+   export let flex = 'flex-row';
+   export let gap = 'gap-4'
+   export let align = 'items-center justify-between'
+   let klass = '';
+   export { klass as class };
+</script>
+
+<div class="w-full flex {flex} {gap} {align} {klass}">
    <slot/>
 </div>
