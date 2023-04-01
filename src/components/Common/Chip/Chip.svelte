fa58910bff97f17cd2894fa48905a1ec375a3c33
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-01 20:50:51 +0200

    Add chip component

---
+++ src/components/Common/Chip/Chip.svelte
@@ -0,0 +1,28 @@
+<script lang="ts">
+   import boxGen from "@styles/mist-theme";
+   import type { ColorProp, VariantProp } from "@styles/theme";
+
+   export const variant: VariantProp = "fill";
+   export const color: ColorProp = "primary";
+
+   let klass = ''
+   export { klass as class }
+
+   const chipStyle = boxGen.full({
+      variant: variant,
+      color: color,
+      size: 'sm',
+      hover: true,
+      active: false,
+      style: {
+         rounded: 'circle',
+         shadow: 'sm',
+         block: false,
+      },
+      classes: klass
+   })
+</script>
+
+<span class="{chipStyle} cursor-default font-semibold">
+   <slot/>
+</span>
