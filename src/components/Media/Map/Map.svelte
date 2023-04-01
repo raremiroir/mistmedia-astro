c9ca2ca6db99e0015de342779255856504c1a358
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-01 15:57:29 +0200

    Fix styles

--- src/components/Media/Map/Map.svelte
+++ src/components/Media/Map/Map.svelte
@@ -1,37 +1,43 @@
 <script>
+   import { onClassChange } from "@utils/observer";
    import { onMount } from "svelte";
-
-   import map_dark from "@styles/components/map_dark";
+   import { mapStyle } from "@styles/components/map";
+   import { ADDRESS } from "@const/index";
    
    let container;
    let map;
    let zoom = 9;
-   let center = {lat: 51.22424678676408, lng: 4.410601398443491};
+   let center = {lat: ADDRESS.lat, lng: ADDRESS.lng};
 
    onMount(() => {
+      // Change map style on theme change
+      const rootEl = document.documentElement;
+      onClassChange(rootEl, (node) => {
+         node.classList.contains('dark')
+            ? map.setOptions({styles: mapStyle('dark')})
+            : map.setOptions({styles: mapStyle('light')})
+      })
+
+      // Initialize map
       map = new google.maps.Map(container, {
-         zoom,
-         center,
-         styles: map_dark,
+         zoom, center, 
+         styles: mapStyle(rootEl.classList.contains('dark') ? 'dark' : 'light'), 
          disableDefaultUI: true,
          zoomControl: true,
          zoomControlOptions: {
-            position: google.maps.ControlPosition.LEFT_BOTTOM
+            position: google.maps.ControlPosition.LEFT_BOTTOM,
+            style: google.maps.ZoomControlStyle.SMALL
          },
          mapTypeControl: true,
          mapTypeControlOptions: {
-            position: google.maps.ControlPosition.RIGHT_BOTTOM
+            position: google.maps.ControlPosition.RIGHT_BOTTOM,
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            mapTypeIds: ['roadmap', 'satellite', 'hybrid', 'terrain']
          },
          scaleControl: true,
          streetViewControl: false,
-         streetViewControlOptions: {
-            position: google.maps.ControlPosition.LEFT_CENTER
-         },
-         fullscreenControl: false,
-         fullscreenControlOptions: {
-            position: google.maps.ControlPosition.LEFT_CENTER
-         }
       })
+
    })
 
 </script>
