95a22b320b3df5319ecf5c39c1d0fdd51ec5cdd2
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-01 20:50:43 +0200

    Add label props + add tags

--- src/components/Common/Card/Card.svelte
+++ src/components/Common/Card/Card.svelte
@@ -3,14 +3,20 @@
    import Heading from '../../Common/Heading/Heading.svelte'
    import boxGen from '../../../styles/mist-theme'
    import type { ColorProp, VariantProp, SizeProp, RoundedProp, ShadowProp } from '../../../styles/theme';
+   import Chip from '../Chip/Chip.svelte';
 
    export let article:boolean = false;
    export let href:string = ''
    export let title:string = '';
+   export let label:string = '';
+   export let labelledBy:string = '';
 
+   export let tags:string[] = [];
    export let footer:boolean = false;
 
    export let direction:'col'|'row' = 'col';
+   export let centerRow:boolean = false;
+   export let noClamp:boolean = false;
 
    export let variant:VariantProp = 'fill';
    export let color:ColorProp = 'surface_alt';
@@ -40,22 +46,24 @@
       classes: klass
    });
 
-   const transition = 'transition duration-300 ease-in-out';
+   const transition = 'transition-all duration-300 ease-in-out';
 
    // Anchor or Div
    $: outerWrapComp = href ? Anchor : Div;
-   $: outerWrapProps = href ? { href } : {}
    // Article or Div
    $: innerWrapComp = article ? Article : Div;
 </script>
 
 
 <svelte:component 
-   this={outerWrapComp} {outerWrapProps} {...$$props} 
+   this={outerWrapComp} {...$$props} 
+   {href}
    tabindex="0"
+   aria-label={label}
+   aria-labelledby={labelledBy}
    title=""
    class="
-      antialiased
+      antialiased {transition} 
       { cardStyle } backdrop-blur-sm
       overflow-hidden group relative
       { fillHeight ? 'h-full' : '' }
@@ -66,27 +74,38 @@
       class="
          flex z-2 {transition}
          h-full
-         { direction === 'row' ? 'flex-row items-center justify-start pl-6 gap-2' : 'flex-col'}
+         { direction === 'row' ? `flex-row ${ centerRow ? 'ml-6' : '!items-start !justify-start'} gap-2` : 'flex-col'}
    ">
       <!-- Image -->
       <slot name="media"/>
       
       <!-- Content -->
-      <div class="flex flex-col gap-4 justify-between h-full { direction === 'row' ? 'p-6' : 'p-8' }">
+      <div class="h-full flex flex-col gap-4 justify-between { direction === 'row' ? 'p-6' : 'p-8' }">
          <div class="flex flex-col gap-4">
             <!-- Header -->
             <header class="">
+               {#if tags}
+                  <div class="flex flex-row gap-2 mb-4">
+                     {#each tags as tag}
+                        <Chip>{tag}</Chip>
+                     {/each}
+                  </div>
+               {/if}
                <slot name="title">
                   <Heading 
                      type="h3" size="xs" fontfam='font-body'
-                     color="{ boxGen.color(color, variant, hover, active) } !bg-transparent !shadow-none">
+                     color="{ boxGen.color(color, variant, hover, active) } {transition} !bg-transparent !shadow-none">
                      { title }
                   </Heading>
                </slot>
             </header>
             
             <!-- Body -->
-            <section class="{ article ? 'line-clamp-4' : ''}">
+            <section class="
+                     { article ? noClamp ? '' : 'line-clamp-4' : ''} {transition}
+                     text-surface-900 dark:text-surface-50
+                     group-hover:text-black dark:group-hover:text-white
+                     font-medium dark:font-normal">
                <slot>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia dignissimos recusandae, quos 
                   accusantium, sed voluptates nobis qui facilis ad sint maxime? Cupiditate tempora nesciunt dignissimos 
@@ -97,7 +116,7 @@
    
          <!-- Footer -->
          {#if footer}
-            <footer class="card-footer w-full p-0 mt-8">
+            <footer class="card-footer w-full p-0 flex items-center justify-end">
                <slot name="footer"/>
             </footer>
          {/if}
