d58e44ab104adc9ebb842697d754ee8a2ab6d288
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-01 03:40:41 +0200

    Add form for contact page

---
+++ src/components/Pages/Contact/ContactForm.svelte
@@ -0,0 +1,55 @@
+<script>
+   import FormRow from "@comp/Form/FormRow.svelte";
+   import Input from "@comp/Form/Input.svelte";
+</script>
+
+<form class="w-full flex flex-col gap-4 py-4">
+
+   <!-- Name -->
+   <FormRow>
+      <!-- First Name -->
+      <Input 
+         label="Given Name" 
+         name="first_name" 
+         placeholder="e.g. John"
+      />
+      <!-- Last Name -->
+      <Input 
+         label="Surname" 
+         name="last_name" 
+         placeholder="e.g. Doe"
+      />
+   </FormRow>
+   
+   <!-- Email -->
+   <FormRow>
+      <Input 
+         label="Email" 
+         name="email" 
+         placeholder="e.g. john.doe@mail.com"
+         type="email"
+      />
+   </FormRow>
+   
+   <!-- Organization -->
+   <FormRow>
+      <Input 
+         label="Organization" 
+         name="organization" 
+         placeholder="e.g. John Doe Inc."
+         type="text"
+      />
+   </FormRow>
+
+   <!-- Subject -->
+   <FormRow>
+      <Input 
+         label="Subject" 
+         name="subject" 
+         placeholder="e.g. I have a question about..."
+         type="text"
+      />
+   </FormRow>
+
+
+</form>
