dd8d5441ada4765e61de8bce7bf83c9bfe0f9916
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-03 01:03:31 +0200

    Add menu component

---
+++ src/components/Common/Menu/Menu.svelte
@@ -0,0 +1,18 @@
+<script lang='ts'>
+   import type { PopupSettings } from '@skeletonlabs/skeleton';
+   import Popup from "@comp/Utils/Popup/Popup.svelte";
+
+   export let id: string;
+   export let placement:PopupSettings['placement'] = 'bottom';
+   export let event:PopupSettings['event'] = 'click';
+</script>
+
+
+
+<Popup {id} {placement} {event}>
+   <slot name="trigger"/>
+
+   <div slot="popup">
+      <slot/>
+   </div>
+</Popup>
