5f7d0d6f275254e7a5833356e57118e3380c812a
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-02 21:24:01 +0200

    Remove creative cloud

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -37,6 +37,11 @@ export const TECHSTACK: {[key:string]: {[key:string]: string}} = {
       svg: 'assets/svg/brandstech/adobe-premierepro.svg',
       url: 'https://www.adobe.com/products/premiere.html'
    },
+   // creativecloud: {
+   //    name: 'Adobe Creative Cloud',
+   //    svg: 'assets/svg/brandstech/adobe-cc.svg',
+   //    url: 'https://www.adobe.com/creativecloud.html'
+   // },
    figma: {
       name: 'Figma',
       svg: 'assets/svg/brandstech/figma.svg',
@@ -67,26 +72,26 @@ export const TECHSTACK: {[key:string]: {[key:string]: string}} = {
       svg: 'assets/svg/brandstech/mdn.svg',
       url: 'https://developer.mozilla.org/en-US/'
    },
-   // nodejs: {
-   //    name: 'Node.js',
-   //    svg: 'assets/svg/brandstech/nodejs.svg',
-   //    url: 'https://nodejs.org/en/'
-   // },
-   // npm: {
-   //    name: 'npm',
-   //    svg: 'assets/svg/brandstech/npm.svg',
-   //    url: 'https://www.npmjs.com/'
-   // },
+   nodejs: {
+      name: 'Node.js',
+      svg: 'assets/svg/brandstech/nodejs.svg',
+      url: 'https://nodejs.org/en/'
+   },
+   npm: {
+      name: 'npm',
+      svg: 'assets/svg/brandstech/npm.svg',
+      url: 'https://www.npmjs.com/'
+   },
    // git: {
    //    name: 'Git',
    //    svg: 'assets/svg/brandstech/git.svg',
    //    url: 'https://git-scm.com/'
    // },
-   // github: {
-   //    name: 'GitHub',
-   //    svg: 'assets/svg/brandstech/github.svg',
-   //    url: 'https://github.com/'
-   // },
+   github: {
+      name: 'GitHub',
+      svg: 'assets/svg/brandstech/github.svg',
+      url: 'https://github.com/'
+   },
    vscode: {
       name: 'Visual Studio Code',
       svg: 'assets/svg/brandstech/vs-code.svg',
@@ -97,17 +102,16 @@ export const TECHSTACK: {[key:string]: {[key:string]: string}} = {
       svg: 'assets/svg/brandstech/google-analytics.svg',
       url: 'https://analytics.google.com/analytics/web/'
    },
-   // google_ads: {
-   //    name: 'Google Ads',
-   //    svg: 'assets/svg/brandstech/google-ads.svg', 
-   //    url: 'https://ads.google.com/home/'
-   // },
-   // google_search_console: {
-   //    name: 'Google Search Console',
-   //    svg: 'assets/svg/brandstech/google-search-console.svg',
-   //    url: 'https://search.google.com/search-console/about'
-   // },
-
+   google_ads: {
+      name: 'Google Ads',
+      svg: 'assets/svg/brandstech/google-ads.svg', 
+      url: 'https://ads.google.com/home/'
+   },
+   google_search_console: {
+      name: 'Google Search Console',
+      svg: 'assets/svg/brandstech/google-search-console.svg',
+      url: 'https://search.google.com/search-console/about'
+   },
    svelte: {
       name: 'Svelte',
       svg: 'assets/svg/brandstech/svelte.svg',
@@ -118,11 +122,11 @@ export const TECHSTACK: {[key:string]: {[key:string]: string}} = {
    //    svg: 'assets/svg/brandstech/react.svg',
    //    url: 'https://reactjs.org/'
    // },
-   // vue: {
-   //    name: 'Vue',
-   //    svg: 'assets/svg/brandstech/vue.svg',
-   //    url: 'https://vuejs.org/'
-   // },
+   vue: {
+      name: 'Vue',
+      svg: 'assets/svg/brandstech/vue.svg',
+      url: 'https://vuejs.org/'
+   },
    astro: {
       name: 'Astro',
       svg: 'assets/svg/brandstech/astro.svg',
@@ -138,36 +142,46 @@ export const TECHSTACK: {[key:string]: {[key:string]: string}} = {
    //    svg: 'assets/svg/brandstech/vite.svg',
    //    url: 'https://vitejs.dev/'
    // },
-   // vercel: {
-   //    name: 'Vercel',
-   //    svg: 'assets/svg/brandstech/vercel.svg',
-   //    url: 'https://vercel.com/'
-   // },
+   vercel: {
+      name: 'Vercel',
+      svg: 'assets/svg/brandstech/vercel.svg',
+      url: 'https://vercel.com/'
+   },
+   gpt: {
+      name: 'OpenAI GPT',
+      svg: 'assets/svg/brandstech/gpt.svg',
+      url: 'https://openai.com/'
+   },
    wordpress: {
       name: 'WordPress',
       svg: 'assets/svg/brandstech/wordpress.svg',
       url: 'https://wordpress.org/'
    },
-   // php: {
-   //    name: 'PHP',
-   //    svg: 'assets/svg/brandstech/php.svg',
-   //    url: 'https://www.php.net/'
-   // },
-   // laravel: {
-   //    name: 'Laravel',
-   //    svg: 'assets/svg/brandstech/laravel.svg',
-   //    url: 'https://laravel.com/'
-   // },
-   // mysql: {
-   //    name: 'MySQL',
-   //    svg: 'assets/svg/brandstech/mysql.svg',
-   //    url: 'https://www.mysql.com/'
-   // },
-   // postgresql: {
-   //    name: 'PostgreSQL',
-   //    svg: 'assets/svg/brandstech/postgresql.svg',
-   //    url: 'https://www.postgresql.org/'
-   // },
+   php: {
+      name: 'PHP',
+      svg: 'assets/svg/brandstech/php.svg',
+      url: 'https://www.php.net/'
+   },
+   laravel: {
+      name: 'Laravel',
+      svg: 'assets/svg/brandstech/laravel.svg',
+      url: 'https://laravel.com/'
+   },
+   mysql: {
+      name: 'MySQL',
+      svg: 'assets/svg/brandstech/mysql.svg',
+      url: 'https://www.mysql.com/'
+   },
+   postgresql: {
+      name: 'PostgreSQL',
+      svg: 'assets/svg/brandstech/postgresql.svg',
+      url: 'https://www.postgresql.org/'
+   },
+   firebase: {
+      name: 'Firebase',
+      svg: 'assets/svg/brandstech/firebase.svg',
+      url: 'https://firebase.google.com/'
+   },
    unity: {
       name: 'Unity',
       svg: 'assets/svg/brandstech/unity.svg',
@@ -183,19 +197,19 @@ export const TECHSTACK: {[key:string]: {[key:string]: string}} = {
    //    svg: 'assets/svg/brandstech/threejs.svg',
    //    url: 'https://threejs.org/'
    // },
-   // unreal: {
-   //    name: 'Unreal Engine',
-   //    svg: 'assets/svg/brandstech/unreal.svg',
-   //    url: 'https://www.unrealengine.com/'
-   // },
-   // csharp: {
-   //    name: 'C#',
-   //    svg: 'assets/svg/brandstech/csharp.svg',
-   //    url: 'https://docs.microsoft.com/en-us/dotnet/csharp/'
-   // },
-   // python: {
-   //    name: 'Python',
-   //    svg: 'assets/svg/brandstech/python.svg',
-   //    url: 'https://www.python.org/'
-   // },
+   csharp: {
+      name: 'C#',
+      svg: 'assets/svg/brandstech/csharp.svg',
+      url: 'https://docs.microsoft.com/en-us/dotnet/csharp/'
+   },
+   python: {
+      name: 'Python',
+      svg: 'assets/svg/brandstech/python.svg',
+      url: 'https://www.python.org/'
+   },
+   jquery: {
+      name: 'jQuery',
+      svg: 'assets/svg/brandstech/jquery.svg',
+      url: 'https://jquery.com/'
+   },
 }
