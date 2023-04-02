c03a2f4864db2646cc95cd1dc2847a79145440eb
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-03 01:04:21 +0200

    Rename popup and add props

--- src/components/Utils/Popup/Popup.svelte
+++ src/components/Utils/Popup/Popup.svelte
@@ -3,13 +3,16 @@
    import type { PopupSettings } from '@skeletonlabs/skeleton';
 
    export let id: string;
-   export let text: string;
 
    export let placement: PopupSettings['placement'] = 'top';
+   export let event: PopupSettings['event'] = 'hover';
+   export let role:string = '';
+
+   role = role?? event === 'hover' ? 'tooltip' : 'dialog';
 
 
    let popupSettings: PopupSettings = {
-      event: 'hover',
+      event: event,
       target: id,
       placement: placement,
    }
@@ -20,7 +23,7 @@
 </div>
 
 <div 
-   data-popup={id} 
+   data-popup={id} {role}
    class="
       arrow
       bg-primary-300/60 dark:bg-primary-900/60
@@ -28,5 +31,6 @@
       px-4 py-2 rounded-lg
       font-semibold
 ">
-   {text}
+   <div class="absolute w-2 h-2 rotate-45"/>
+   <slot name="popup"/>
 </div>
