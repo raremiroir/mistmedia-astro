a06521fec8471b8e207bdd5cc28078ab35809c40
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-02 12:48:39 +0200

    Edit widths

--- src/consts/class.ts
+++ src/consts/class.ts
@@ -1,5 +1,5 @@
 
-export const widths =  'mx-auto w-full sm:w-[95%] md:w-11/12 lg:w-5/6 xl:w-4/5 2xl:w-3/4';
+export const widths =  'mx-auto w-full sm:w-[95%] md:w-11/12 lg:w-5/6 xl:w-3/4 2xl:w-2/3';
 
 export const slider = `
       w-full h-full max-h-120
