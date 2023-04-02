5b4a30598c70859746aa53cca6bb97d6909dcf34
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-03 01:03:39 +0200

    Wrap header in li

--- src/components/Main/Header/HeaderLink.svelte
+++ src/components/Main/Header/HeaderLink.svelte
@@ -10,15 +10,17 @@ export { klass as class};
 $: active = href === currentPath || href === currentPath.replace(/\/$/, '');
 </script>
 
-<Button
-	href={href}
-	klass="{klass}"
-	variant="minimal"
-	color="surface_alt"
-	shadow="none"
-	size="md"
-	{active} flat tile ripple
-	{...$$props}
->
-	<slot />
-</Button>
+<li class="unstyles list-none p-0 m-0">
+	<Button
+		href={href}
+		klass="{klass}"
+		variant="minimal"
+		color="surface_alt"
+		shadow="none"
+		size="md"
+		{active} flat tile ripple
+		{...$$props}
+	>
+		<slot />
+	</Button>
+</li>
