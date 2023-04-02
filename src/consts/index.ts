53929c4b8005c8e223e7c2a3339717e7527f85e5
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-02 12:48:44 +0200

    Add icons

--- src/consts/index.ts
+++ src/consts/index.ts
@@ -9,3 +9,14 @@ export const ADDRESS = {
    lat: 51.22424678676408, 
    lng: 4.410601398443491
 }
+
+export const SERVICES: {[key:string]: {[key:string]: string}} = {
+   icons: {
+      web:    'ph:globe-simple-duotone',
+      design: 'ph:paint-bucket-duotone',
+      seo:    'ph:rocket-launch-duotone',
+      social: 'ph:megaphone-simple-duotone',
+      av:     'ph:film-slate-duotone',
+      '3d':   'ph:cube-duotone',
+   }
+}
