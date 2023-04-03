181f2923534adf5daa822a7ca4ed6a7d75b70252
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-03 03:02:15 +0200

    Add button to trigger

--- src/components/Common/Menu/Menu.svelte
+++ src/components/Common/Menu/Menu.svelte
@@ -1,6 +1,8 @@
 <script lang='ts'>
    import type { PopupSettings } from '@skeletonlabs/skeleton';
    import Popup from "@comp/Utils/Popup/Popup.svelte";
+   import Button from '@comp/Common/Button/Button.svelte';
+   import Icon from '@iconify/svelte';
 
    export let id: string;
    export let placement:PopupSettings['placement'] = 'bottom';
@@ -9,8 +11,19 @@
 
 
 
-<Popup {id} {placement} {event}>
-   <slot name="trigger"/>
+<Popup 
+   {id} {placement} {event}
+   bg="bg-gray-50 dark:bg-surface-500 shadow-lg shadow-black/30"
+   padding="p-2"
+>
+   <div class="">
+      <Button variant="minimal" color="surface_alt" shadow="none" size="md" block flat tile ripple>
+         <div class="flex gap-2 items-center justify-center">
+            <slot name="trigger"/>
+            <Icon icon="ph:caret-down-fill" class="w-3 h-3 mb-0.5 text-inherit"/>
+         </div>
+      </Button>
+   </div>
 
    <div slot="popup">
       <slot/>
