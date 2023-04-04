d0f8175dfd68f711f510ace4ad347f86acd82d78
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-04 03:40:55 +0200

    Add mailgun backend

---
+++ src/pages/api/mail.ts
@@ -0,0 +1,71 @@
+import type { APIRoute } from "astro";
+
+import Mailgun from "mailgun-js";
+
+const MAILGUN_API_KEY = import.meta.env.MAILGUN_API_KEY;
+const MAILGUN_DOMAIN = import.meta.env.MAILGUN_DOMAIN;
+const MAILGUN_BASE_URL = import.meta.env.MAILGUN_BASE_URL;
+
+const mailgun = new Mailgun({
+   apiKey: MAILGUN_API_KEY,
+   domain: MAILGUN_DOMAIN,
+   host: MAILGUN_BASE_URL,
+});
+
+
+const sender = {
+   name: 'Mist Media',
+   email: '',
+}
+const recipients = [
+   'mirostorm@gmail.com',
+   'miro.storm@trixolutions.be',
+]
+const cc = 'miro.polfliet@student.kdg.be'
+
+export const post: APIRoute = async ({ request }) => {
+   const { subject, message } = await request.json();
+
+   // console.log('arrived at endpoint: ', subject, message);
+   try{
+      const data = await mail.send(
+         subject,  
+         message,
+         sender,
+         recipients,
+         cc,
+      );
+      return new Response(
+         JSON.stringify({ message: 'Email sent successfully', data }), 
+         { status: 200 })
+   } catch (err) {
+      return new Response(
+         JSON.stringify({ message: 'Failed to send email', err }), 
+         { status: 500 })
+   }
+}
+
+const mail = {
+   send: async(
+      subject: string, 
+      message: string,
+      sender: { name: string, email: string } = { name: 'Mist Media', email: ''},
+      recipients: string[] = ['mirostorm@gmail.com', 'miro@mistmedia.be'],
+      cc: string[]|string = '',
+   ) => {
+      try {
+         const data = {
+            from: `${sender.name} <noreply@${MAILGUN_DOMAIN}>`,
+            to: recipients,
+            cc: cc,
+            subject: subject,
+            text: message,
+            html: `<p>${message}</p>`,
+         }
+         const res = mailgun.messages().send(data);
+         // console.log('res: ', res);
+      } catch (err) {
+         console.error('err: ', err);
+      }
+   }
+}
