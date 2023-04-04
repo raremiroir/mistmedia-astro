44d344d178a5f744c01f4633b3f78a01c2b31de7
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-04 14:48:42 +0200

    Add types for form

---
+++ src/types/components/form.d.ts
@@ -0,0 +1,54 @@
+
+
+// Email
+export interface SendEmailProps {
+   from: string;
+   to: string[];
+   subject: string;
+   message: string;
+   cc?: string[]|string;
+}
+
+// Input types
+export type InputItem = {
+   enabled: boolean;
+   disabled?: boolean;
+   placeholder?: string;
+   required?: boolean;
+}
+export type InputItems = InputItem[];
+
+export type TextareaItem = InputItem & {
+   rows?: number;
+}
+export type SelectItem = InputItem & {
+   options: string[];
+}
+
+// Chosen form inputs
+export type ChosenInputs = {
+   name?: InputItem;
+   email?: InputItem;
+   phone?: InputItem;
+
+   company?: InputItem;
+   job?: InputItem;
+
+   subject?: InputItem;
+   message?: TextareaItem;
+   file?: InputItem;
+
+   address?: InputItem;
+   city?: InputItem;
+   state?: InputItem;
+   zip?: InputItem;
+   country?: SelectItem;
+
+   date?: InputItem;
+   time?: InputItem;
+   text?: InputItem;
+   number?: InputItem;
+
+   turnstile_response?: InputItem;
+   accept_terms?: InputItem;
+}
