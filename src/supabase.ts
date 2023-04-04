75324c3cf8888e1d8ddd4f8ca2e9bdbbfa59c950
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-04 02:56:00 +0200

    Add possible future supabase connection

---
+++ src/supabase.ts
@@ -0,0 +1,5 @@
+import { createClient } from '@supabase/supabase-js'
+// import type { Database } from './types'
+
+// No need to connect as of yet
+// https://www.aleksandra.codes/astro-supabase#supabase-project-initialization
