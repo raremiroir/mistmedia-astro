7dc9ff30425a0d4c7e0073bad6f64b0a3ed3d6c8
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-04 02:30:07 +0200

    Update store to svelte store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,8 +1,9 @@
 import { atom } from 'nanostores'
+import { writable } from 'svelte/store';
 /* ~~-~~ ~-~~-~ ~~-~~ */
 /* ~~-~~ STORES ~~-~~ */
 /* ~~-~~ ~-~~-~ ~~-~~ */
 
 
 // Components
-export const animationTrigger = atom(false);
+export const animationTrigger = writable(false);
