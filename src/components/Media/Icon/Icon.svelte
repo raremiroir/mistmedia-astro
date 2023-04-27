8e25901289eff797317c3417f39723aa5ec5fde8
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-27 03:20:07 +0200

    Add icon component

---
+++ src/components/Media/Icon/Icon.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+
+   import Iconify from "@iconify/svelte";
+
+   // import BoldDuotonePriceTagEuro from "./custom/BoldDuotonePriceTagEuro.svelte";
+   // import BoldDuotoneScale from "./custom/BoldDuotoneScale.svelte";
+	// import BoldDuotoneMoneyBag from "./custom/BoldDuotoneMoneyBag.svelte";
+   import { classes } from "@/consts/class";
+
+   // const customIcons: any = {
+   //    'pricetag': BoldDuotonePriceTagEuro,
+   //    'scale': BoldDuotoneScale,
+   //    'moneybag': BoldDuotoneMoneyBag
+   // }
+
+   export let icon = '';
+   // export let customIcon: string = '';
+
+   export let width: string = "w-8";
+   export let color: string = "text-primary-500";
+   export let hex: string = "#deece8";
+
+   let klass = '';
+   export { klass as class };
+</script>
+
+
+<!-- {#if customIcon !== ''}
+   <svelte:component 
+      this={customIcons[customIcon]}
+      fillColor="{hex}"
+      {width}
+   />
+{:else} -->
+   <Iconify icon={icon} class="{width} h-auto {color} {classes.transition.default} {klass}" />
+<!-- {/if} -->
