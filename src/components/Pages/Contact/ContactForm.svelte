a4a3369262f6ff77c3a1fa56492786e1aaef7e60
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-27 02:23:50 +0200

    Base contact form on new component

--- src/components/Pages/Contact/ContactForm.svelte
+++ src/components/Pages/Contact/ContactForm.svelte
@@ -1,79 +1,87 @@
-<script>
-   import Button from "@comp/Common/Button/Button.svelte";
-import FormRow from "@comp/Form/FormRow.svelte";
-   import Input from "@comp/Form/Input.svelte";
-   import TextArea from "@comp/Form/TextArea.svelte";
-   import ToggleCheck from "@comp/Form/ToggleCheck.svelte";
+<script lang="ts">
+   import { t } from "i18next";
+   
+	import type { ChosenInputs } from "@/types/components/form";
+   import FormBase from "@comp/Form/FormBase.svelte";
+
+   import { mailToAdmin } from "@/utils/mail";
+
+   let inputItems: ChosenInputs = {
+      name: { enabled: true, required: true, },
+      email: { enabled: true, required: true, },
+      organisation: { enabled: true, required: false, },
+      subject: { enabled: true, required: true, },
+      message: { enabled: true, required: true, rows: 6, },
+      accept_terms: { enabled: true, required: true },
+      turnstile_response: { enabled: true, required: true },
+   }
+
+
+   $: translations = {
+      name: {
+         first_name: t('form.input.first_name'),
+         last_name: t('form.input.last_name'),
+      },
+      email: t('form.input.email'),
+      phone: t('form.input.phone'),
+      organisation: t('form.input.organisation'),
+      job: t('form.input.job'),
+      subject: t('form.input.subject'),
+      message: t('form.input.message'),
+      accept_terms: t('form.accept_terms'),
+      turnstile_response: t('form.turnstile_response'),
+      validation: {
+         required: t('form.validation.required'),
+         only_alpha: t('form.validation.only_alpha'),
+         field_too_short: t('form.validation.field_too_short'),
+         field_too_long: t('form.validation.field_too_long'),
+         email_error: t('form.validation.email_error'),
+         phone_error: t('form.validation.phone_error'),
+         terms_error: t('form.validation.terms_error'),
+         turnstile_error: t('form.validation.captcha_error'),
+      },
+      placeholder: {
+         first_name: t('form.placeholders.first_name'),
+         last_name: t('form.placeholders.last_name'),
+         email: t('form.placeholders.email'),
+         phone: t('form.placeholders.phone'),
+         organisation: t('form.placeholders.organisation'),
+         job: t('form.placeholders.job'),
+         subject: t('form.placeholders.subject'),
+         message: t('form.placeholders.message'),
+      },
+      alert: {
+         title: t('form.input.submit.send.success'),
+         message: t('form.input.submit.send.message'),
+         button: t('form.input.submit.send.another_message'),
+      }
+   } 
 </script>
 
-<form method="post" class="w-full flex flex-col gap-4 py-4">
+<FormBase 
+   {translations}
+   {inputItems}
+   title={`${t("contact.cta")}`}
+   submitText={`${t("form.input.submit.default")}`}
+   submitAction={(values) => {
 
-   <!-- Name -->
-   <FormRow>
-      <!-- First Name -->
-      <Input 
-         label="Given Name" 
-         name="first_name" 
-         placeholder="e.g. John"
-      />
-      <!-- Last Name -->
-      <Input 
-         label="Surname" 
-         name="last_name" 
-         placeholder="e.g. Doe"
-      />
-   </FormRow>
-   
-   <!-- Email -->
-   <FormRow>
-      <Input 
-         label="Email" 
-         name="email" 
-         placeholder="e.g. john.doe@mail.com"
-         type="email"
-      />
-   </FormRow>
-   
-   <!-- Organization -->
-   <FormRow>
-      <Input 
-         label="Organization" 
-         name="organization" 
-         placeholder="e.g. John Doe Inc."
-      />
-   </FormRow>
-
-   <!-- Subject -->
-   <FormRow>
-      <Input 
-         label="Subject" 
-         name="subject" 
-         placeholder="e.g. I have a question about..."
-      />
-   </FormRow>
-
-   <!-- Message -->
-   <FormRow>
-      <TextArea 
-         label="Message" 
-         name="message" 
-         placeholder="Your message here..."
-         rows="8" noResize
-      />
-   </FormRow>
-
-   <!-- Accept terms -->
-   <FormRow>
-      <ToggleCheck 
-         label="I have read and accept the privacy policy and terms of service." 
-         name="accept_terms" 
-         required size="sm"
-      />
-   </FormRow>
-
-   <FormRow align="justify-end items-center">
-      <Button type="submit" variant="fill" color="surface_dark" size="lg">
-         Send Message
-      </Button>
-   </FormRow>
-</form>
+      const subject = `Trixolutions.be: Nieuw bericht van ${values.first_name} ${values.last_name} - ${values.subject}`;
+      const message = `
+         <b><i>Nieuw bericht van:</i></b><br/>
+         Voornaam: <b>${values.first_name}</b><br/>
+         Achternaam: <b>${values.last_name}</b><br/>
+         E-mail: <b>${values.email}</b><br/>
+         ${ values.company ? `Bedrijf: <b>${values.company}</b><br/>` : '' }
+         <h2>${values.subject}</h2>
+         <hr/>
+         <p>${values.message}</p>
+         <br/>
+         <hr/>
+         <i>
+            Dit bericht werd automatisch verzonden vanaf <a href="https://trixolutions.be">Trixolutions.be</a>.<br/>
+            Om te reageren op de verzender, dien je een aparte email naar zijn/haar e-mailadres te sturen.
+         </i>`;
+      // mailToAdmin({ subject, message });
+      console.log({ subject, message });
+   }}
+/>
