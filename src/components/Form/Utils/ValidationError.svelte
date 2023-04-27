5ca74676d7efdb9d8509d1c8cc56c8f9c9d6fd18
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-27 03:20:16 +0200

    Edit transition to slide

--- src/components/Form/Utils/ValidationError.svelte
+++ src/components/Form/Utils/ValidationError.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
    // Import utils
-	import { fade } from "svelte/transition";
+	import { fade, slide } from "svelte/transition";
 
    // Export error
    export let error:any = '';
@@ -9,8 +9,8 @@
 
 <span 
   aria-errormessage={error}
-  class="text-sm text-error italic font-body"
-  transition:fade={{duration:200}}  >
+  class="text-sm text-error italic font-body leading-none"
+  transition:slide={{duration:200, axis: 'y'}}  >
   {error}
 </span>
 
