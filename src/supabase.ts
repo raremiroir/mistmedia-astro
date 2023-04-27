a438e484e3587d107e90d2ff9d9fe52c7988ac6c
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-04-28 00:10:51 +0200

    Remove supabase

--- src/supabase.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from '@supabase/supabase-js'
-// import type { Database } from './types'
-
-// No need to connect as of yet
-// https://www.aleksandra.codes/astro-supabase#supabase-project-initialization
