6ec03f9c8e1cedfd3d9a9ba9104f661ebd8420ab
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-01 12:47:28 +0200

    Add 2xs title size

--- src/types/components/heading.d.ts
+++ src/types/components/heading.d.ts
@@ -1,2 +1,2 @@
-export type TitleSizes = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
-export type TitleTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'subheader' | 'subtitle';
+export type TitleSizes = '2xs' | 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+export type TitleTags = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6';
