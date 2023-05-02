35adf3d98c79ad257a61092d59106c62f3035315
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-02 03:01:31 +0200

    Add locale store

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,9 +1,12 @@
+import type { Locales } from '@/types';
 import { atom } from 'nanostores'
 import type { WritableAtom } from 'nanostores'
 import { type Writable, writable } from 'svelte/store';
 /* ~~-~~ ~-~~-~ ~~-~~ */
 /* ~~-~~ STORES ~~-~~ */
 /* ~~-~~ ~-~~-~ ~~-~~ */
+
+export const locale: WritableAtom<Locales> = atom('nl');
 
 
 export const openMobile: WritableAtom<boolean> = atom(false);
