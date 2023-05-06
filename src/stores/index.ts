c9afb9858c2c80bfad56f1fe154cacec872d5e03
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-06 17:14:19 +0200

    Add comments + stores for modal & tab

--- src/stores/index.ts
+++ src/stores/index.ts
@@ -1,15 +1,42 @@
 import type { Locales } from '@/types';
+import type { ModalSettings } from '@skeletonlabs/skeleton';
 import { atom } from 'nanostores'
 import type { WritableAtom } from 'nanostores'
 import { type Writable, writable } from 'svelte/store';
+import type { MistCase } from '../types/content';
 /* ~~-~~ ~-~~-~ ~~-~~ */
 /* ~~-~~ STORES ~~-~~ */
 /* ~~-~~ ~-~~-~ ~~-~~ */
 
+// Current locale
 export const locale: WritableAtom<Locales> = atom('nl');
 
-
+// Trigger for mobile nav menu
 export const openMobile: WritableAtom<boolean> = atom(false);
-
+// Trigger for drawer component
 export const animationTrigger: Writable<boolean> = writable(false);
+// Active tab in tabs component
+export const currentTab: WritableAtom<number> = atom(0);
+// Errors in file dropzone
 export const fileErrors: Writable<string[]> = writable([]);
+
+
+// Modal settings
+export const modalSettings: WritableAtom<ModalSettings> = atom({
+   type: 'component',
+   component: '',
+   buttonTextCancel: 'Cancel',
+   buttonTextConfirm: 'Confirm',
+   buttonTextSubmit: 'Submit',
+})
+// Active modal in modals component
+export const currentModal: WritableAtom<string|undefined> = atom('caseDetail');
+// Active modal item
+export const currentModalItem: WritableAtom<MistCase> = atom({
+   description: '',
+   enabled: false,
+   images: {
+      card: ''
+   },
+   title: '',
+});
