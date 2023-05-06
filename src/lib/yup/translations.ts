89622b40b68cde092a81ed84e9056de22935a839
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-07 00:27:18 +0200

    Add translations for forms

---
+++ src/lib/yup/translations.ts
@@ -0,0 +1,130 @@
+export const labels = {
+   name: {
+      first_name: {
+         en: 'First Name',
+         nl: 'Voornaam'
+      },
+      last_name: {
+         en: 'Last Name',
+         nl: 'Achternaam'
+      },
+   },
+   email: {
+      en: 'Email',
+      nl: 'E-mail'
+   },
+   phone: {
+      en: 'Phone',
+      nl: 'Telefoon'
+   },
+   organisation: {
+      en: 'Organisation',
+      nl: 'Organisatie'
+   },
+   job: {
+      en: 'Job Title',
+      nl: 'Job Titel'
+   },
+   subject: {
+      en: 'Subject',
+      nl: 'Onderwerp'
+   },
+   message: {
+      en: 'Message',
+      nl: 'Bericht'
+   },
+   accept_terms: {
+      en: 'Accept Terms',
+      nl: 'Accepteer Voorwaarden'
+   },
+   turnstile_response: {
+      en: 'Turnstile Response',
+      nl: 'Turnstile Reactie'
+   },
+   password: {
+      en: 'Password',
+      nl: 'Wachtwoord'
+   },
+}
+export const validation = {
+   required: {
+      en: 'is required',
+      nl: 'is verplicht'
+   },
+   only_alpha: {
+      en: 'can only contain letters',
+      nl: 'kan alleen letters bevatten'
+   },
+   field_too_short: {
+      en: 'is too short',
+      nl: 'is te kort'
+   },
+   field_too_long: {
+      en: 'is too long',
+      nl: 'is te lang'
+   },
+   email_error: {
+      en: 'is not a valid email',
+      nl: 'is geen geldig e-mailadres'
+   },
+   phone_error: {
+      en: 'is not a valid phone number',
+      nl: 'is geen geldig telefoonnummer'
+   },
+   terms_error: {
+      en: 'You must accept the terms and conditions',
+      nl: 'Je moet de algemene voorwaarden accepteren'
+   },
+   turnstile_error: {
+      en: "Just to prove you're not a robot...",
+      nl: 'Om te bewijzen dat je geen robot bent...'
+   },
+};
+export const placeholder = {
+   first_name: {
+      en: 'e.g. John',
+      nl: 'b.v. Jan'
+   },
+   last_name: {
+      en: 'e.g. Smith',
+      nl: 'b.v. Janssen'
+   },
+   email: {
+      en: 'e.g. john.smith@mail.com',
+      nl: 'b.v. jan.janssen@mail.be'
+   },
+   phone: {
+      en: 'e.g. 0123456789',
+      nl: 'b.v. 0123456789'
+   },
+   organisation: {
+      en: 'e.g. Company Ltd.',
+      nl: ''
+   },
+   job: {
+      en: 'e.g. CEO',
+      nl: ''
+   },
+   subject: {
+      en: 'e.g. Sales Enquiry',
+      nl: ''
+   },
+   message: {
+      en: 'Your message here...',
+      nl: ''
+   },
+};
+export const alert = {
+   title: {
+      en: 'Message Sent!',
+      nl: ''
+   },
+   message: {
+      en: 'Thank you for your message, we will get back to you as soon as possible.',
+      nl: ''
+   },
+   button: {
+      en: 'Send another message?',
+      nl: ''
+   },
+}
