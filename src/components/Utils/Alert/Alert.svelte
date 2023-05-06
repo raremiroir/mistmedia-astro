964c3a93229c082001b03cb0e6babb14ae038c6f
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-07 00:26:35 +0200

    Add compact prop

--- src/components/Utils/Alert/Alert.svelte
+++ src/components/Utils/Alert/Alert.svelte
@@ -10,19 +10,24 @@
    export let icon:string = 'material-symbols:warning-rounded'
 
 
+   export let compact:boolean = false;
 </script>
 
 
 {#if visible}
-   <aside 
-      class="alert variant-ghost"
+   <aside class="
+         alert variant-ghost 
+         { compact ? 'flex-col' : 'flex-col md:flex-row'} 
+         items-center justify-between gap-4"
       transition:fade|local={{duration: 200}}>
-      <!-- Icon -->
-      <div><Icon {icon} class="w-8 h-8" /></div>
-      <!-- Message -->
-      <div class="alert-message">
-         <h3>{title}</h3>
-         <p><slot/></p>
+      <div class="flex flex-row items-center justify-center gap-4">
+         <!-- Icon -->
+         <div><Icon {icon} class="w-8 h-8" /></div>
+         <!-- Message -->
+         <div class="alert-message">
+            <h3>{title}</h3>
+            <p><slot/></p>
+         </div>
       </div>
       <!-- Actions -->
       <div class="alert-actions">
