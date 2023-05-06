16755f4a24a703800b4e6d48a0635253ab76b162
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-06 17:11:55 +0200

    Add tags and categories

---
+++ src/components/Pages/Cases/CaseTags.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+   import db from "@/firebase";
+   import Tag from "@/components/Text/Tags/Tag.svelte";
+   import type { DocumentReference } from "firebase/firestore";
+
+
+   export let categories: {
+      id: string;
+      enabled: boolean;
+   }[] = [];
+
+   console.log(categories);
+
+   export let tags: string[] = [];
+</script>
+
+<!-- Tags -->
+   <div class="flex flex-row gap-1">
+      {#each categories as category}
+         {#if category.enabled}
+            <Tag color="bg-primary-500/20 hover:bg-primary-500/60 text-white">
+               {category.id}
+            </Tag>
+         {/if}
+      {/each}
+   </div>
