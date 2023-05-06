a4845292e4756fde5a9bd173683fd260a363c64b
raremiroir <115361462+miropolfliet@users.noreply.github.com>
2023-05-06 17:14:58 +0200

    Add tag component

---
+++ src/components/Text/Tags/Tag.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+   import Icon from "@/components/Media/Icon/Icon.svelte";
+
+
+   export let icon: string = '';
+   export let color: string = 'bg-primary-500 text-white';
+   
+   let klass = '';
+   export { klass as class };
+</script>
+
+<span class="
+   chip {color}
+   {klass}">
+
+   {#if icon}
+      <span>
+         <Icon {icon} color="text-inherit" width="w-3" />
+      </span>
+   {/if}
+
+   <span>
+      <slot/>
+   </span>
+
+</span>
