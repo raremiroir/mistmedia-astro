4ed3d137868837d6437b5f052df212f73a632a58
Miro Storm <mirostorm@gmail.com>
2023-05-17 22:02:11 +0200

    Wrap trigger in slot

--- src/components/Common/Accordeon/AccordeonTrigger.svelte
+++ src/components/Common/Accordeon/AccordeonTrigger.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
    import Title from '@/components/Text/Title/Title.svelte';
 
-   export let title:string;
+   export let title:string = '';
    export let open:boolean = false;
    export let icon = false;
    export let key = 0;
@@ -22,29 +22,31 @@
       aria-controls="accordeon-item-{key}"
       class="w-full flex justify-between items-center"
    >
-      <div class="w-full flex justify-start gap-4 items-center">
-         {#if icon}
-            <div class="w-8 h-8 text-surface-900 group-hover:text-primary-800 dark:text-surface-50 dark:group-hover:text-primary-400 {transition}">
-               <slot name="icon"/>
-            </div>
-         {/if}
-         <Title 
-            h3 size="2xs" family="font-body"
-            color="text-surface-900 dark:text-surface-50 group-hover:text-primary-900 dark:group-hover:text-primary-400 {transition}">
-            {title}
-         </Title>
-      </div>
-      <svg xmlns="http://www.w3.org/2000/svg" 
-            class="
-               w-8 h-8 {transition} 
-               { open ? 'rotate-0' : '-rotate-180' } 
-               text-surface-900 group-hover:text-primary-800 
-               dark:text-surface-50 dark:group-hover:text-primary-400" 
-            viewBox="0 0 256 256">
-         <g fill="currentColor">
-            <path d="M208 160H48l80-80Z" opacity=".2"/>
-            <path d="m213.66 154.34l-80-80a8 8 0 0 0-11.32 0l-80 80A8 8 0 0 0 48 168h160a8 8 0 0 0 5.66-13.66ZM67.31 152L128 91.31L188.69 152Z"/>
-         </g>
-      </svg>
+      <slot>
+         <div class="w-full flex justify-start gap-4 items-center">
+            {#if icon}
+               <div class="w-8 h-8 text-surface-900 group-hover:text-primary-800 dark:text-surface-50 dark:group-hover:text-primary-400 {transition}">
+                  <slot name="icon"/>
+               </div>
+            {/if}
+            <Title 
+               h3 size="2xs" family="font-body"
+               color="text-surface-900 dark:text-surface-50 group-hover:text-primary-900 dark:group-hover:text-primary-400 {transition}">
+               {title}
+            </Title>
+         </div>
+         <svg xmlns="http://www.w3.org/2000/svg" 
+               class="
+                  w-8 h-8 {transition} 
+                  { open ? 'rotate-0' : '-rotate-180' } 
+                  text-surface-900 group-hover:text-primary-800 
+                  dark:text-surface-50 dark:group-hover:text-primary-400" 
+               viewBox="0 0 256 256">
+            <g fill="currentColor">
+               <path d="M208 160H48l80-80Z" opacity=".2"/>
+               <path d="m213.66 154.34l-80-80a8 8 0 0 0-11.32 0l-80 80A8 8 0 0 0 48 168h160a8 8 0 0 0 5.66-13.66ZM67.31 152L128 91.31L188.69 152Z"/>
+            </g>
+         </svg>
+      </slot>
    </div>
 </div>
