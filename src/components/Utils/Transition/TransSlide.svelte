d4735aca497aab02e24d697c7a093d40b20a6ba8
Miro Storm <mirostorm@gmail.com>
2023-05-17 22:03:41 +0200

    Add transition wrap component

---
+++ src/components/Utils/Transition/TransSlide.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+   import { slide } from "svelte/transition";
+
+   export let duration: number = 200;
+   export let axis: "x" | "y" = "y";
+
+   export let visible: boolean = false;
+   $: visible;
+
+</script>
+
+{#if visible}
+   <div
+      in:slide={{delay: duration, duration, axis}}
+      out:slide={{duration, axis}}
+   >
+      <slot />
+   </div>
+{/if}
