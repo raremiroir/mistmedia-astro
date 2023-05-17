2a69f1e65782c77d95ea177c2de4a6f64d91df28
Miro Storm <mirostorm@gmail.com>
2023-05-17 21:57:33 +0200

    Add tags for each prop

--- src/components/Pages/Cases/CaseTags.svelte
+++ src/components/Pages/Cases/CaseTags.svelte
@@ -1,26 +1,50 @@
 <script lang="ts">
+   import { t } from 'i18next'
    import db from "@/firebase";
    import Tag from "@/components/Text/Tags/Tag.svelte";
-   import type { DocumentReference } from "firebase/firestore";
+   import type { DocumentData, DocumentReference } from "firebase/firestore";
 
+   import { locale } from "@/stores";
 
-   export let categories: {
-      id: string;
-      enabled: boolean;
-   }[] = [];
+   // export let categories: DocumentData[];
+   // let categoriesData: {
+   //    id: string;
+   //    enabled: boolean;
+   // }[] = categories.map((category) => {
+   //    return {
+   //       id: category.id,
+   //       enabled: category.enabled,
+   //    };
+   // });
 
-   // console.log(categories);
+   export let clientTag: string;
+   export let solutionTag: string;
 
-   export let tags: string[] = [];
+   export let tags: DocumentData[] = [];
+   // console.log(tags);
+
+   const tagClass = '!text-base backdrop-blur-md'
+
+   const tagColorClient = 'variant-soft bg-secondary-700/40 dark:bg-secondary-700/40 text-secondary-100 dark:text-secondary-100'
+   const tagColorSolution = 'variant-soft bg-primary-700/40 dark:bg-primary-700/40 !text-primary-100 dark:!text-primary-100'
+   const tagColor = 'variant-soft bg-slate-100/40 dark:bg-slate-800/40 text-slate-700 dark:text-slate-100'
 </script>
 
 <!-- Tags -->
    <div class="flex flex-row gap-1">
-      {#each categories as category}
-         {#if category.enabled}
-            <Tag color="bg-primary-500/20 hover:bg-primary-500/60 text-white">
-               {category.id}
-            </Tag>
-         {/if}
+      <Tag class="{tagClass}" color="{tagColorClient}">
+         {clientTag}
+      </Tag>
+      <Tag 
+         class="{tagClass}" 
+         color="{tagColorSolution}"
+         element="a" href="/solutions/{solutionTag}"
+      >
+         {t(`services:solutions.${solutionTag}.title_short`)}
+      </Tag>
+      {#each tags as tag}
+         <Tag class="{tagClass}" color="{tagColor}">
+            {tag.title[$locale]}
+         </Tag>
       {/each}
    </div>
