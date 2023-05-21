307e7d5b7941a52714a172928f474afcb721c5b2
Miro Storm <mirostorm@gmail.com>
2023-05-21 02:47:17 +0200

    Reformat

--- src/components/Pages/Home/HomePortfolioSlider.svelte
+++ src/components/Pages/Home/HomePortfolioSlider.svelte
@@ -1,61 +1,82 @@
 <script lang="ts">
-   import { Splide, SplideSlide } from '@splidejs/svelte-splide';
-   import type { SliderProps } from "component-slider";
-   import { slider } from "@/consts/style";
+   import { Splide, SplideSlide, SplideTrack } from '@splidejs/svelte-splide';
+   // import type { SliderProps } from "component-slider";
 
-   import SlidePage from "@/components/Common/Slider/SlidePage.svelte";
    import Card from "@/components/Common/Card/Card.svelte";
 
    const label = 'Portfolio Slider';
-
-   const sliderOptions = {
-      label: label,
-      pagination: true,
-      cover: true,
-      type: 'loop',
-      focus: 'center',
-      drag: true,
-      snap: true,
-      gap: '1rem',
-      autoplay: 'pause',
-      interval: 5000,
-      speed: 300,
-      omitEnd: true,
-      updateOnMove: true,
-      height: 200,
-      width: 'fit-content',
-      autoWidth: true,
-   }
 </script>
 
 <Splide 
-   class="{slider}"
-   options={{sliderOptions}} 
-   aria-label="{label}"
-   classes={{
-      pagination: 'splide__pagination slide-pagination',
-      page: 'splide__pagination__page slide-page bg-surface-400'
-   }}>
-   <SlidePage>
-      <SlideBase>
-         <img src="placeholder-about.jpg" alt="1" class="object-cover w-96 h-auto"/>
-      </SlideBase>
-      <SlideBase>
-         <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
-      </SlideBase>
-      <SlideBase>
-         <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
-      </SlideBase>
-   </SlidePage>
-   <SlidePage>
-      <SlideBase>
-         <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
-      </SlideBase>
-      <SlideBase>
-         <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
-      </SlideBase>
-      <SlideBase>
-         <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
-      </SlideBase>
-   </SlidePage>
+   options={{
+      label,
+      perPage: 3,
+      perMove: 2,
+      type: 'loop',
+      gap: '1rem',
+      breakpoints: {
+         1280: {
+            perPage: 4,
+            perMove: 2,
+         },
+         1024: {
+            perPage: 3,
+            perMove: 1,
+         },
+         768: {
+            perPage: 2,
+            perMove: 1,
+         },
+         640: {
+            perPage: 1,
+            perMove: 1,
+         },
+      }
+   }} 
+   aria-label="{label}">
+   <SplideSlide>
+      <Card 
+         block article 
+         color="surface_alt" 
+         hover active media="custom">
+         <div slot="media">
+            <img src="placeholder-about.jpg" alt="1" class="object-cover w-96 h-auto"/>
+         </div>
+      </Card>
+   </SplideSlide>
+   <SplideSlide>
+      <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
+   </SplideSlide>
+   <SplideSlide>
+      <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
+   </SplideSlide>
+   <SplideSlide>
+      <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
+   </SplideSlide>
+   <SplideSlide>
+      <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
+   </SplideSlide>
+   <SplideSlide>
+      <img src="placeholder-hero.jpg" alt="2" class="object-cover w-96 h-auto"/>
+   </SplideSlide>
 </Splide>
+
+
+<style global lang="postcss">
+   .splide__pagination {
+      bottom: 0;
+      left: 0;
+      right: 0;
+      margin: 0 auto;
+      width: fit-content;
+      height: 2rem;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      gap: 0.5rem;
+      padding: 0.5rem;
+      background-color: red;
+      border-radius: 0.5rem;
+   }
+
+</style>
