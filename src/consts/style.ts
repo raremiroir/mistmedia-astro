ae5cb0f3fbaa8b8e780a1b6f342f541f556b8c2c
Miro Storm <mirostorm@gmail.com>
2023-05-28 18:28:39 +0200

    Add gaps2

--- src/consts/style.ts
+++ src/consts/style.ts
@@ -1,6 +1,7 @@
 
 export const widths =  'mx-auto w-full sm:w-[95%] md:w-11/12 xl:w-5/6 2xl:w-3/4';
 export const gaps = 'gap-2 md:gap-3 lg:gap-4 xl:gap-6 2xl:gap-8';
+export const gaps_2 = 'gap-3 md:gap-4 xl:gap-6 2xl:gap-8';
 
 export const slider = `
       w-full h-full max-h-120
