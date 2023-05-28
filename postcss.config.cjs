b136696109c40ded56c0de60af9f1011e1dbb8d0
Miro Storm <mirostorm@gmail.com>
2023-05-28 18:56:43 +0200

    Remove unused postcss plugins

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,9 +2,6 @@ module.exports = {
    plugins: [
       require('postcss-import'),
       require('@tailwindcss/nesting')(require('postcss-nested')),
-      require('postcss-simple-vars'),
-      require('postcss-each'),   // might cause recompile issues
-      require('postcss-mixins'),
       require('tailwindcss'),
       require('autoprefixer'),
    ]
