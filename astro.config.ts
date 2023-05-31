bb3291f910ee36d836d6cddb21cb3b9e4a214841
Miro Storm <mirostorm@gmail.com>
2023-05-31 17:24:43 +0200

    Define new components

--- astro.config.ts
+++ astro.config.ts
@@ -11,13 +11,63 @@ import robotsTxt from "astro-robots-txt";
 import mdx from '@astrojs/mdx';
 import prefetch from '@astrojs/prefetch';
 import storyblok from '@storyblok/astro';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 import { vitePreprocess } from '@astrojs/svelte';
+
+import { loadEnv } from 'vite'
+const env = loadEnv("", process.cwd(), 'STORYBLOK')
 
 // https://astro.build/config
 const config: AstroUserConfig = {
   site: 'https://mistmedia.be',
   // Integrations
   integrations: [
+    storyblok({
+      accessToken: "Lx4wOhjB1cWELPQMNi02xwtt",// env.STORYBLOK_TOKEN,
+      apiOptions: {
+        region: "eu",
+        cache: { clear: 'auto', type: 'memory' },
+      },
+      enableFallbackComponent: true,
+      components: {
+        text: "storyblok/Text",
+        html: "storyblok/Html",
+        // Layouts
+        page: "components/Core/Page/Page",
+        page_solution: "layouts/SolutionLayout",
+        // Core
+        section: "storyblok/Core/Section",
+        box: "storyblok/Core/Box",
+        div: "storyblok/Core/Div",
+        grid: "storyblok/Core/Grid",
+        flex: "storyblok/Core/Flex",
+        // Utils
+        link: "storyblok/Utils/Link",
+        typewriter: "storyblok/Utils/Typewriter",
+        typewriter_multiple: "storyblok/Utils/TypewriterMultiple",
+        // Common
+        button: "storyblok/Common/Button",
+        button_social: "storyblok/Common/ButtonSocial",
+        hero_about: "storyblok/Common/Heros/HeroAbout",
+        hero_home: "storyblok/Common/Heros/HeroHome",
+        hero: "storyblok/Common/Heros/Hero",
+        card_hover: "storyblok/Common/Cards/CardHover",
+        card_static: "storyblok/Common/Cards/CardStatic",
+        card_testimonial: "storyblok/Common/Cards/CardTestimonial",
+        card_portfolio: "storyblok/Common/Cards/CardPortfolio",
+        accordeon: "storyblok/Common/Accordeon/Accordeon",
+        accordeon_item: "storyblok/Common/Accordeon/AccordeonItem",
+        drawer: "storyblok/Common/Drawer",
+        slider_home: "storyblok/Common/SliderHome",
+        case_tag: "storyblok/Common/Case/Tag",
+        case_tags: "storyblok/Common/Case/Tags",
+        // Media
+        image_1: "storyblok/Media/Image/Image_1",
+        image_16_9: "storyblok/Media/Image/Image_16-9",
+        logo_icon_anim: "storyblok/Media/Logo/LogoIconAnim",
+        logo_lettering: "storyblok/Media/Logo/LogoLettering",
+      }
+    }),
     sitemap({
       i18n: {
         defaultLocale: 'nl',
@@ -49,19 +99,17 @@ const config: AstroUserConfig = {
       }
     }),
     robotsTxt(),
-    storyblok({
-      accessToken: "Lx4wOhjB1cWELPQMNi02xwtt",
-      apiOptions: {
-        region: "eu",
-      },
-      components: {
-        page: "components/Core/Page/Page.astro",
-      }
-    })
   ],
-  output: "server",
+  output: "hybrid",
+  experimental: {
+    hybridOutput: true
+  },
   adapter: vercel(),
   vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
     define:{
       'process.env.VITE_BUILD_TIME':JSON.stringify(new Date().toISOString()),
     },  
