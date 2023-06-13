8260186ec99a418292672c87afe7bdce7d116cfb
Miro Storm <mirostorm@gmail.com>
2023-06-14 01:17:03 +0200

    Add preset types

--- src/components/Utils/Alert/Alert.svelte
+++ src/components/Utils/Alert/Alert.svelte
@@ -7,8 +7,30 @@
    export let visible:boolean = false;
 
    export let title:string;
-   export let icon:string = 'material-symbols:warning-rounded'
+   export let icon:string = ''
 
+   export let type: 'info' | 'success' | 'warning' | 'error' = 'warning';
+
+   const iconName:string = (icon !== '' && !!icon) ? icon 
+      : type === 'info' ? 'ph:info-duotone'
+      : type === 'success' ? 'ph:check-circle-duotone'
+      : type === 'warning' ? 'ph:warning-duotone'
+      : type === 'error' ? 'ph:x-circle-duotone'
+      : 'mdi:alert-circle-outline';
+
+   const bgColor:string = 
+      type === 'info' ? 'bg-tertiary-100-800-token' 
+      : type === 'success' ? 'bg-success-100-800-token' 
+      : type === 'warning' ? 'bg-warning-100-800-token' 
+      : type === 'error' ? 'bg-error-100-800-token' 
+      : 'bg-surface-100-800-token';
+   
+   const textColor:string =
+      type === 'info' ? 'text-tertiary-900-50-token'
+      : type === 'success' ? 'text-success-900-50-token'
+      : type === 'warning' ? 'text-warning-900-50-token'
+      : type === 'error' ? 'text-error-900-50-token'
+      : 'text-surface-900-50-token';
 
    export let compact:boolean = false;
 </script>
@@ -16,13 +38,13 @@
 
 {#if visible}
    <aside class="
-         alert variant-ghost 
+         alert variant-ghost {bgColor} {textColor}
          { compact ? 'flex-col' : 'flex-col md:flex-row'} 
          items-center justify-between gap-4"
       transition:fade|local={{duration: 200}}>
       <div class="flex flex-row items-center justify-center gap-4">
          <!-- Icon -->
-         <div><Icon {icon} class="w-8 h-8" /></div>
+         <div><Icon icon={iconName} class="w-8 h-8" /></div>
          <!-- Message -->
          <div class="alert-message">
             <h3>{title}</h3>
